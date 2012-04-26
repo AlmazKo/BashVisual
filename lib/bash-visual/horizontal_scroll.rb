8b092e5be1e99f558748f73d9bb81f3b3257b303
Alexander Suslov <a.s.suslov@gmail.com>
2012-04-26 23:18:41 +0400

    refactoring projects structure #6

--- lib/bash-visual/horizontal_scroll.rb
+++ lib/bash-visual/horizontal_scroll.rb
@@ -1,69 +1,69 @@
-require 'scroll'
+module Bash_Visual
+  class HorizontalScroll < Scroll
 
-class HorizontalScroll < Scroll
-  
-  DEFAULT_SEPARATOR = "\u2502"
-  
-  def initialize options
-    super
-    @message_block_width = @message_block_size
+    DEFAULT_SEPARATOR = "\u2502"
 
-    @separator = 
-      if @separator == true;                  DEFAULT_SEPARATOR
-      elsif @separator.instance_of? String;   @separator[0]
-      else                                    nil
-    end
-  end
-  
-  def get_x_position available_area_width, message_width = 0
-    if (@start == BEGINNING)
-      @x + (@area_width - available_area_width)
-    else
-      @x + available_area_width - message_width
-    end
-  end
-  
-  def print_message message, font, available_area
-    
-    available_area_width, available_area_height = available_area
-    avail_width = @message_block_width > available_area_width ? available_area_width : @message_block_width
-    
-    
-    (available_area_height - message.size).times {
-      message << ''
-    }
-    
-    if @adapt_size_message
-      message_width = rows_wrap! message, available_area_width
-    else
-      rows_wrap! message, avail_width
-      message_width = avail_width
-    end
-    
-    message = message.slice(0, available_area_height) if message.size > available_area_height
-    
-    message.map! do |row|
-        row.strip.ljust(message_width)
-    end
-    
-    if @separator and @separator.size.nonzero?
-      if @adapt_size_message
-        if message_width < available_area_width
-          message.map! do |row|
-              row << @separator
-          end
-          message_width += 1
-        end
-      else
-        message.each do |row|
-            row[message_width-1] = @separator
-        end
+    def initialize options
+      super
+      @message_block_width = @message_block_size
+
+      @separator =
+        if @separator == true;                  DEFAULT_SEPARATOR
+        elsif @separator.instance_of? String;   @separator[0]
+        else                                    nil
       end
     end
 
-    write(get_x_position(available_area_width, message_width), @y, message, font)
+    def get_x_position available_area_width, message_width = 0
+      if (@start == BEGINNING)
+        @x + (@area_width - available_area_width)
+      else
+        @x + available_area_width - message_width
+      end
+    end
 
-    [available_area_width - message_width, available_area_height]
+    def print_message message, font, available_area
+
+      available_area_width, available_area_height = available_area
+      avail_width = @message_block_width > available_area_width ? available_area_width : @message_block_width
+
+
+      (available_area_height - message.size).times {
+        message << ''
+      }
+
+      if @adapt_size_message
+        message_width = rows_wrap! message, available_area_width
+      else
+        rows_wrap! message, avail_width
+        message_width = avail_width
+      end
+
+      message = message.slice(0, available_area_height) if message.size > available_area_height
+
+      message.map! do |row|
+          row.strip.ljust(message_width)
+      end
+
+      if @separator and @separator.size.nonzero?
+        if @adapt_size_message
+          if message_width < available_area_width
+            message.map! do |row|
+                row << @separator
+            end
+            message_width += 1
+          end
+        else
+          message.each do |row|
+              row[message_width-1] = @separator
+          end
+        end
+      end
+
+      write(get_x_position(available_area_width, message_width), @y, message, font)
+
+      [available_area_width - message_width, available_area_height]
+    end
+
   end
-  
 end
--- lib/bash-visual/scroll.rb
+++ lib/bash-visual/scroll.rb
@@ -1,120 +1,117 @@
-$:.unshift(File.dirname(__FILE__))
+module Bash_Visual
+ class Scroll
 
-require 'console'
+    FORWARD  = 1
+    BACKWARD = -1
 
-class Scroll
-  
-  FORWARD  = 1
-  BACKWARD = -1
-  
-  BEGINNING = false
-  ENDING    = true
-  
-  attr_accessor :console 
-  
-  def initialize(options)
+    BEGINNING = false
+    ENDING    = true
 
-    @x, @y = options[:coordinates]
-    @x = @x.to_i
-    @y = @y.to_i
+    attr_accessor :console
 
-    raise ':window_size isn\'t array' unless options[:window_size].instance_of? Array
-    raise 'size :window_size must be great than 1' if options[:window_size].size < 2
-      
-    @area_width, @area_height = options[:window_size]
-    @area_width = @area_width.to_i
-    @area_height = @area_height.to_i
+    def initialize(options)
 
-    @message_block_size = options[:message_block_size] ? options[:message_block_size].to_i : 1
-    @message_block_size = 1 if @message_block_size < 1
-   
-    @prefix = options[:prefix] ? options[:prefix] : nil
-    
-    @adapt_size_message = options[:adapt_size_message] ? options[:adapt_size_message] : false
-    
-    @is_wrap = true
-    @start = options[:start] ? ENDING : BEGINNING
-    @separator = options[:separator] ? options[:separator] : false
-    @font = options[:font] ? options[:font] : nil
+      @x, @y = options[:coordinates]
+      @x = @x.to_i
+      @y = @y.to_i
 
-    @stack = []
-    @console = Console.new @font,Console::OUTPUT_STRING
-    @mutex = Mutex.new
-  end
-  
-  def scroll(positions = 1, direction = @direction * positions)
+      raise ':window_size isn\'t array' unless options[:window_size].instance_of? Array
+      raise 'size :window_size must be great than 1' if options[:window_size].size < 2
 
-  end
-  
-  def add (message, font = @font)
-    
-    if @stack.size.zero? 
-      print @console.draw_rectangle(@x + 1, @y + 1, @area_width, @area_height, font)
+      @area_width, @area_height = options[:window_size]
+      @area_width = @area_width.to_i
+      @area_height = @area_height.to_i
+
+      @message_block_size = options[:message_block_size] ? options[:message_block_size].to_i : 1
+      @message_block_size = 1 if @message_block_size < 1
+
+      @prefix = options[:prefix] ? options[:prefix] : nil
+
+      @adapt_size_message = options[:adapt_size_message] ? options[:adapt_size_message] : false
+
+      @is_wrap = true
+      @start = options[:start] ? ENDING : BEGINNING
+      @separator = options[:separator] ? options[:separator] : false
+      @font = options[:font] ? options[:font] : nil
+
+      @stack = []
+      @console = Console.new @font,Console::OUTPUT_STRING
+      @mutex = Mutex.new
     end
-    
-    @stack << {
-      message: prefix() << message.to_s,
-         font: font
-    }
 
-    redraw()
-    #@stack.slice!(-index, index)
-  end
-  
-  def prefix= (prefix)
-    @prefix = prefix
-  end
-  
-  def prefix
-    if (defined? @prefix.call)
-      @prefix[].to_s
-    else
-      ''
+    def scroll(positions = 1, direction = @direction * positions)
+
     end
-  end
 
-  private
+    def add (message, font = @font)
 
-  def redraw
-    avail_area = [@area_width, @area_height]
-    @stack.reverse.each do |item|
-      
-      message = item[:message].dup.lines.to_a
-      font = item[:font]
-      unless font.background
-        font = Font.new font.font, font.foreground, @font.background
+      if @stack.size.zero?
+        print @console.draw_rectangle(@x + 1, @y + 1, @area_width, @area_height, font)
       end
 
-      avail_area = print_message(message, font, avail_area)
-      # выходим если закончилось место в области
-      return nil if avail_area[0] <= 0 or avail_area[1] <= 0
+      @stack << {
+        message: prefix() << message.to_s,
+           font: font
+      }
+
+      redraw()
+      #@stack.slice!(-index, index)
     end
-  end
-  
-  # сделать переносы в массиве строк
-  def rows_wrap! arr, max_len
-    max_used_len = 1
-    arr.each_with_index do |row, i| 
-      len = row.size
-      max_used_len = len if len > max_used_len
-      next if len <= max_len
-      tail = row.slice!(max_len,  len-max_len)
-      arr.insert(i+1, tail)
+
+    def prefix= (prefix)
+      @prefix = prefix
     end
-    max_used_len
-  end
-  
-  def write (x, y, message, font)
+
+    def prefix
+      if (defined? @prefix.call)
+        @prefix[].to_s
+      else
+        ''
+      end
+    end
+
+    private
+
+    def redraw
+      avail_area = [@area_width, @area_height]
+      @stack.reverse.each do |item|
+
+        message = item[:message].dup.lines.to_a
+        font = item[:font]
+        unless font.background
+          font = Font.new font.font, font.foreground, @font.background
+        end
+
+        avail_area = print_message(message, font, avail_area)
+        # выходим если закончилось место в области
+        return nil if avail_area[0] <= 0 or avail_area[1] <= 0
+      end
+    end
+
+    # сделать переносы в массиве строк
+    def rows_wrap! arr, max_len
+      max_used_len = 1
+      arr.each_with_index do |row, i|
+        len = row.size
+        max_used_len = len if len > max_used_len
+        next if len <= max_len
+        tail = row.slice!(max_len,  len-max_len)
+        arr.insert(i+1, tail)
+      end
+      max_used_len
+    end
+
+    def write (x, y, message, font)
 
 
-    string = ''
-    message.each_with_index { |text, i|   
-     string << @console.write_to_position(x, y + i, text, font)
-    }
-      
-    @mutex.synchronize {
-      print string
-    }
+      string = ''
+      message.each_with_index { |text, i|
+       string << @console.write_to_position(x, y + i, text, font)
+      }
+
+      @mutex.synchronize {
+        print string
+      }
+    end
   end
 end
-
