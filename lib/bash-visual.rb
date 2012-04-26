48aa91ce8edee230a0fb1e568cfa7c34bf972068
Alexander Suslov <a.s.suslov@gmail.com>
2012-04-26 22:24:01 +0400

    refactoring projects structure ....

--- lib/bash-visual.rb
+++ lib/bash-visual.rb
@@ -1,3 +1,10 @@
+require 'bash-visual/version'
+require 'bash-visual/console'
+require 'bash-visual/font'
+require 'bash-visual/scroll'
+require 'bash-visual/builder'
+require 'bash-visual/horizontal_scroll'
+require 'bash-visual/vertical_scroll'
 
 module Bash_Visual
 
